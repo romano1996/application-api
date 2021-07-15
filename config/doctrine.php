496ca09ae87240f6d0fdae637ed94e781367eb95
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-07-15 09:49:59 -0700

    Create application entities mapping.

--- config/doctrine.php
+++ config/doctrine.php
@@ -31,7 +31,7 @@ return [
             'connection'    => env('DB_CONNECTION', 'mysql'),
             'namespaces'    => [],
             'paths'         => [
-                base_path('app/Entities')
+                base_path('domain/Application/Entities')
             ],
             'repository'    => Doctrine\ORM\EntityRepository::class,
             'proxies'       => [
---
+++ domain/Application/Entities/Application.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Application\Entities;
+
+class Application
+{
+
+}
