ef384377cd3f09bf10b067a26dccd86e052ff347
Romano Schoonheim <romano@romanoschoonheim.nl>
2021-07-15 09:43:21 -0700

    Doctrine install

--- config/app.php
+++ config/app.php
@@ -175,6 +175,14 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Doctrine & Doctrine extensions
+         */
+        \LaravelDoctrine\ORM\DoctrineServiceProvider::class,
+        \LaravelDoctrine\Migrations\MigrationsServiceProvider::class,
+        \LaravelDoctrine\Extensions\GedmoExtensionsServiceProvider::class,
+        \LaravelDoctrine\Extensions\BeberleiExtensionsServiceProvider::class,
+
     ],
 
     /*
@@ -227,7 +235,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'EntityManager' => LaravelDoctrine\ORM\Facades\EntityManager::class,
+        'Registry'      => LaravelDoctrine\ORM\Facades\Registry::class,
+        'Doctrine'      => LaravelDoctrine\ORM\Facades\Doctrine::class,
     ],
 
 ];
